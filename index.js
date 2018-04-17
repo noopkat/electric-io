096d35a0d62ebf7778aa887f4fca605070b74c5a
noopkat <suz.hinton@gmail.com>
2018-04-16 22:29:14 -0400

    send 500 if saving in locked mode

--- index.js
+++ index.js
@@ -75,8 +75,8 @@ app.get('/api/dashboard', function (req, res) {
 });
 
 app.post('/api/dashboard', function (req, res) {
-  if (editMode === 'locked') return;
   console.log('dashboard save requested');
+  if (editMode === 'locked') return res.sendStatus(500);
   const fileContents = `{ "dashboard": ${JSON.stringify(req.body)} }`;
   fs.writeFile(__dirname + '/.data/dashboard.json', fileContents, 'utf8', (error) => {
     if (error) return res.sendStatus(500);
