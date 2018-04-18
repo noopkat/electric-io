484198e35f304dc4fa2ac514cfb3080ce7099dd2
noopkat <suz.hinton@gmail.com>
2018-04-18 19:29:09 -0400

    remove forgotten commented code

--- public/js/application.js
+++ public/js/application.js
@@ -1,13 +1,6 @@
 import app from './components/app.js';
-//import initWebsocket  from './lib/websocket.js';
-
-// create a global event bus for socket io events
-//Vue.prototype.$bus = new Vue({});
 
 const vm = new Vue({
   el: '#app',
-  
 });
-
-//initWebsocket(vm);
 
