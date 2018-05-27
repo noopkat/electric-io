e6a398fe2b767144d492ad7aeebddf2f64f34d8e
Rhian van Esch <rhian.vanesch@gmail.com>
2018-05-27 20:42:55 +0100

    Add open/close functionality and styles to settings panel

--- public/js/components/settings.js
+++ public/js/components/settings.js
@@ -3,34 +3,42 @@ import templates from '../lib/templates.js';
 import createGuid from '../lib/guid.js';
 
 const template = `
-  <div class="card" id="settings">
-    <h2>Settings</h2>
-    <form v-on:submit.prevent="onSaveSettings">
-      <label>Background Color
-        <input type="text" name="bgColor" v-bind:value="dashboard.bgColor" />
-      </label>
-      <label>Background Image URL
-        <input type="text" name="bgImageUrl" :value="dashboard.bgImageUrl" />
-      </label>
-      <label class="settings__checkbox-label">
-        <span>Repeat background image?</span>
-        <input class="settings__checkbox" type="checkbox" name="bgImageRepeat" :value="dashboard.bgImageRepeat" v-model="dashboard.bgImageRepeat" />
-      </label>
-      <input type="submit" value="save"/>
-    </form>
-    <h3>New Card</h3>
-    <form v-on:submit.prevent="onCreateCard">
-      <label>Card Type
-        <select name="type">
-          <option value="button">button</option>
-          <option value="lineChart">line chart</option>
-          <option value="number">number</option>
-          <option value="sticker">sticker</option>
-          <option value="text">text</option>
-        </select>
-      </label>
-      <input type="submit" value="create"/>
-    </form>
+  <div class="card settings" :class="{'settings--closed': !settingsPanelOpen}">
+    <div class="settings__header">
+      <button class="settings__toggle-btn" @click="onToggleSettingsPanel" :aria-expanded="settingsPanelOpen ? 'true' : 'false'" aria-labelledby="settings-toggle-label">
+        <span class="screen-reader-only" id="settings-toggle-label">{{ settingsPanelOpen ? 'Close settings panel' : 'Open settings panel' }}</span>
+        <span aria-hidden="true">{{ settingsPanelOpen ? '&rarr;' : '⚙️' }}</span>
+      </button>
+      <h2 class="settings__title">Settings</h2>
+    </div>
+    <div class="settings__body">
+      <form v-on:submit.prevent="onSaveSettings">
+        <label>Background Color
+          <input type="text" name="bgColor" v-bind:value="dashboard.bgColor" />
+        </label>
+        <label>Background Image URL
+          <input type="text" name="bgImageUrl" :value="dashboard.bgImageUrl" />
+        </label>
+        <label class="settings__checkbox-label">
+          <span>Repeat background image?</span>
+          <input class="settings__checkbox" type="checkbox" name="bgImageRepeat" :value="dashboard.bgImageRepeat" v-model="dashboard.bgImageRepeat" />
+        </label>
+        <input type="submit" value="save"/>
+      </form>
+      <h3>New Card</h3>
+      <form v-on:submit.prevent="onCreateCard">
+        <label>Card Type
+          <select name="type">
+            <option value="button">button</option>
+            <option value="lineChart">line chart</option>
+            <option value="number">number</option>
+            <option value="sticker">sticker</option>
+            <option value="text">text</option>
+          </select>
+        </label>
+        <input type="submit" value="create"/>
+      </form>
+    </div>
   </div>
 `;
 
@@ -39,7 +47,9 @@ export default Vue.component('dashboard-settings', {
   props: ['dashboard'],
   data: function() {
     return {
-      status: ''
+      status: '',
+      settingsPanelOpen: true,
+      temp: '➡️'
     }
   },
   methods: {
@@ -59,6 +69,9 @@ export default Vue.component('dashboard-settings', {
       const newTile = Object.assign({}, tileTemplate, {id});
 
       this.$emit('tile-create', newTile);
+    },
+    onToggleSettingsPanel() {
+      this.settingsPanelOpen = this.settingsPanelOpen === true ? false : true;
     }
   }
 });
