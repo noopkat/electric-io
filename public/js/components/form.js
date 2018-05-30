9057eb665921d5b70e3095f42302e5c47b90c215
Alex Lewis <alexmichael.lewis@gmail.com>
2018-05-29 22:32:25 -0400

    Revert "refactor(form): Make form card more declarative"
    This reverts commit 5e29301ecc9c05ce3e24c0d8310cb6b484cdf254.

--- public/js/components/form.js
+++ public/js/components/form.js
@@ -1,19 +1,65 @@
-// focus management mixin
-import FormFields from './formFields';
-import { focus } from '../vendor/vue-focus.js';
+/* 
+*** WARNING ****
+This file is a hot mess. It could do with some love and refactoring. - noopkat
 
+*/
+import {saveDashboard} from '../lib/configuration.js';
+// focus management mixin
+import {focus} from '../vendor/vue-focus.js';
+
+// TODO:
+// what I'd like to see here instead is to use a declarative object template for each card
+// each object specifies which fields it needs and the name, etc
+// we can look these up based on the tile's properties itself, so it should be a neat refactor?
+// title and id are constant fields we always need, along with the 'save' button
+// this would clean up the entire component file immensely!
+// - noopkat
 const template = `
   <div class="cardForm">
     <h2>Settings</h2>
 
     <form v-on:submit.prevent="onSubmit">
-      <input name="id" type="hidden" :value="tile.id"/>
+      <input name="id" type="hidden" v-bind:value="tile.id"/>
 
       <label>Title
-        <input v-focus="editing" type="text" name="title" :value="tile.title"/>
+        <input v-focus="editing" type="text" name="title" v-bind:value="tile.title"/>
       </label>
 
-      <form-fields :tile="tile" :deviceList="deviceList" />
+      <label v-if="showDeviceList">Device Id
+        <select name="deviceId" id="deviceSelect" >
+            <option v-for="device in deviceList" v-bind:selected="device===tile.deviceId">
+              {{device}}
+            </option>
+         </select>
+       </label>
+      
+      <label v-if="showPropInput">Data Property
+        <input type="text" name="property" v-bind:value="tile.property"/>
+      </label>
+
+      <label v-if="showMethodInput">Method Name
+        <input type="text" name="deviceMethod" v-bind:value="tile.deviceMethod"/>
+      </label>
+
+      <label v-if="showColorInput">Line Color
+        <input type="text" name="lineColor" v-bind:value="tile.lineColor"/>
+      </label>
+
+      <label v-if="showUrlInput">Picture URL
+        <input type="text" name="url" v-bind:value="tile.url"/>
+      </label>
+
+      <label v-if="showButtonTextInput">Button Text
+        <input type="text" name="buttonText" v-bind:value="tile.buttonText"/>
+      </label>
+
+      <label v-if="showTextColorInput">Text Color
+        <input type="text" name="textColor" v-bind:value="tile.textColor"/>
+      </label>
+
+      <label v-if="showMarkdownInput">This supports <a href="https://guides.github.com/features/mastering-markdown/" target="_blank">markdown</a>!
+          <textarea name="tileText" :value="tile.tileText" ></textarea>
+      </label>
 
       <input type="submit" value="save" />
     </form>
@@ -21,10 +67,35 @@ const template = `
 `;
 
 export default Vue.component('card-form', {
-  directives: { focus },
-  components: { FormFields },
+  directives: {focus},
   template,
-  props: ['tile', 'deviceList', 'editing'],
+  props: ["tile", "deviceList", "editing"],
+  computed: {
+    showPropInput: function() {
+      return this.tile.type !== "sticker" && this.tile.type !== "button" && this.tile.type !== "text";
+    },
+    showMethodInput: function() {
+      return this.tile.type === "button";
+    },
+    showColorInput: function() {
+      return this.tile.type === "lineChart";
+    },
+    showDeviceList: function() {
+      return this.tile.type !== "sticker" && this.tile.type !== "text";
+    },
+    showUrlInput: function() {
+      return this.tile.type === "sticker";
+    },
+    showButtonTextInput: function() {
+      return this.tile.type === "button";
+    },
+    showTextColorInput: function() {
+      return this.tile.type === "number";
+    },
+    showMarkdownInput: function() {
+      return this.tile.type === "text";
+    }
+  },
   methods: {
     onSubmit: function(event) {
       let eventData = {};
@@ -34,5 +105,6 @@ export default Vue.component('card-form', {
       });
       this.$emit('save-settings', eventData);
     }
-  }
+  },
 });
+
--- public/js/components/formFields.js
+++
@@ -1,6 +0,0 @@
-export default Vue.component('form-fields', {
-  props: ['tile', 'deviceList'],
-  created: function() {
-    this.$options.template = this.$options.propsData.tile.template;
-  }
-});
