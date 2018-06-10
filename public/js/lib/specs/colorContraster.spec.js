213d01704f35e97a5cffe4cb70aa3eea82296d66
Colin Tindle <ampersand@underscore.space>
2018-06-10 22:28:21 +0100

    tabs > spaces

--- public/js/lib/specs/colorContraster.spec.js
+++ public/js/lib/specs/colorContraster.spec.js
@@ -2,37 +2,37 @@ import colorContrastCheck from '../colorContraster'
 
 describe('colorContrast checker', () => {
 
-	describe('with color names', () => {
-		test('Return dark colour given light', () => {
-			expect(colorContrastCheck('papayawhip', 'white', 'black')).toBe('black')
-		})
-		
-		test('Return light colour given dark', () => {
-			expect(colorContrastCheck('purple', 'white', 'black')).toBe('white')
-		})
-	})
+  describe('with color names', () => {
+    test('Return dark colour given light', () => {
+      expect(colorContrastCheck('papayawhip', 'white', 'black')).toBe('black')
+    })
+    
+    test('Return light colour given dark', () => {
+      expect(colorContrastCheck('purple', 'white', 'black')).toBe('white')
+    })
+  })
 
-	describe('with hex colours', () => {
-		test('Return dark colour given light', () => {
-			expect(colorContrastCheck('#ffefd5', 'white', 'black')).toBe('black')
-		})
+  describe('with hex colours', () => {
+    test('Return dark colour given light', () => {
+      expect(colorContrastCheck('#ffefd5', 'white', 'black')).toBe('black')
+    })
 
-		test('Return light colour given dark', () => {
-			expect(colorContrastCheck('#800080', 'white', 'black')).toBe('white')
-		})
-	})
+    test('Return light colour given dark', () => {
+      expect(colorContrastCheck('#800080', 'white', 'black')).toBe('white')
+    })
+  })
 
-	describe('with rgb color', () => {
-		test('Return dark colour given light', () => {
-			expect(colorContrastCheck('rgb(255,239,213)', 'white', 'black')).toBe('black')
-		})
+  describe('with rgb color', () => {
+    test('Return dark colour given light', () => {
+      expect(colorContrastCheck('rgb(255,239,213)', 'white', 'black')).toBe('black')
+    })
 
-		test('Return light colour given dark', () => {
-			expect(colorContrastCheck('rgb(128,0,128)', 'white', 'black')).toBe('white')
-		})
-	})
+    test('Return light colour given dark', () => {
+      expect(colorContrastCheck('rgb(128,0,128)', 'white', 'black')).toBe('white')
+    })
+  })
 
-	test('with no colours passed in', () => {
-		expect(colorContrastCheck()).toBe('#000000')
-	})
+  test('with no colours passed in', () => {
+    expect(colorContrastCheck()).toBe('#000000')
+  })
 });
