fa06dc56cfd9843d59a18f5081d79b7d7c5ea98c
Paul DeCarlo <pdecarlo@microsoft.com>
2018-09-22 00:13:47 -0500

    Conditionally configure socket.io

--- index.js
+++ index.js
@@ -4,21 +4,22 @@
 // 2. put routes handers in a file
 // 3. put iot related work in a file
 //
-require('dotenv').config();
-var express = require('express');
-var fs = require('fs');
-var http = require('http');
-var debug = require('debug')('server');
-var socket = require('socket.io');
-var helpers = require('./lib/helpers');
-var liveHub = require('./lib/liveHub');
-var simSub = require('./lib/simHub');
-var Simulator = require('./lib/simulator/runSimulator');
+require("dotenv").config();
+var express = require("express");
+var fs = require("fs");
+var http = require("http");
+var debug = require("debug")("server");
+var socket = require("socket.io");
+var helpers = require("./lib/helpers");
+var liveHub = require("./lib/liveHub");
+var simSub = require("./lib/simHub");
+var Simulator = require("./lib/simulator/runSimulator");
 
 var simulating = process.env.SIMULATING;
 var connectionString = process.env.CONNECTION_STRING;
-var consumerGroup = process.env.CONSUMER_GROUP || '$Default';
-var editMode = process.env.EDIT_MODE || 'unlocked';
+var consumerGroup = process.env.CONSUMER_GROUP || "$Default";
+var editMode = process.env.EDIT_MODE || "unlocked";
+var platform = process.env.PLATFORM || "default";
 
 const receiverOptions = {
   connectionString,
@@ -26,17 +27,19 @@ const receiverOptions = {
 };
 
 // TODO: this can be deduped and written more cleverly
-if (simulating === 'true') {
-  var {registry, client, Receiver} = simSub(connectionString);
+if (simulating === "true") {
+  var { registry, client, Receiver } = simSub(connectionString);
   var receiver = new Receiver(receiverOptions);
   const simulator = new Simulator(receiver);
   simulator.start();
 } else {
   if (!connectionString) {
-    console.error(new Error("oops, you're missing a CONNECTION_STRING entry in ./.env!"));
+    console.error(
+      new Error("oops, you're missing a CONNECTION_STRING entry in ./.env!")
+    );
     process.exit();
   }
-  var {registry, client, Receiver} = liveHub(connectionString);
+  var { registry, client, Receiver } = liveHub(connectionString);
   var receiver = new Receiver(receiverOptions);
 }
 
@@ -44,81 +47,102 @@ var port = process.env.PORT || 3000;
 
 var app = express();
 var server = http.createServer(app);
-var io = require('socket.io')(server,{
-  perMessageDeflate :false
-});
+var io;
+if (platform === "azure") {
+  io = require("socket.io")(server, {
+    perMessageDeflate: false
+  });
+} else {
+  io = socket(server);
+}
 
 helpers.createDashboardFileIfNotExists(function() {
-  server.listen(port, function () {
-    console.log('Server listening at port %d', port);
+  server.listen(port, function() {
+    console.log("Server listening at port %d", port);
   });
 });
 
 // Routing
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(express.json());
 
-app.get('/api/devices/list', function (req, res) {
-  debug('device list requested');
+app.get("/api/devices/list", function(req, res) {
+  debug("device list requested");
   registry.list((error, list) => {
-    var names = list.map((d) => d.deviceId);
+    var names = list.map(d => d.deviceId);
     debug(error, names);
-    res.send(names)
+    res.send(names);
   });
 });
 
-app.get('/api/dashboard', function (req, res) {
-  debug('dashboard requested');
-  fs.readFile(__dirname + '/.data/dashboard.json', {encoding: 'utf8'}, (err, data) => {
-    debug(data, err);
-    let jsonData = JSON.parse(data);
-    jsonData.dashboard.editMode = editMode;
-    res.send(JSON.stringify(jsonData));    
-  });
+app.get("/api/dashboard", function(req, res) {
+  debug("dashboard requested");
+  fs.readFile(
+    __dirname + "/.data/dashboard.json",
+    { encoding: "utf8" },
+    (err, data) => {
+      debug(data, err);
+      let jsonData = JSON.parse(data);
+      jsonData.dashboard.editMode = editMode;
+      res.send(JSON.stringify(jsonData));
+    }
+  );
 });
 
-app.post('/api/dashboard', function (req, res) {
-  debug('dashboard save requested');
-  const excludedModes = ['locked', 'demo'];
+app.post("/api/dashboard", function(req, res) {
+  debug("dashboard save requested");
+  const excludedModes = ["locked", "demo"];
   if (excludedModes.includes(editMode)) return res.sendStatus(500);
   const fileContents = `{ "dashboard": ${JSON.stringify(req.body)} }`;
-  fs.writeFile(__dirname + '/.data/dashboard.json', fileContents, 'utf8', (error) => {
-    if (error) return res.sendStatus(500);
-    res.sendStatus(200);
-  });
+  fs.writeFile(
+    __dirname + "/.data/dashboard.json",
+    fileContents,
+    "utf8",
+    error => {
+      if (error) return res.sendStatus(500);
+      res.sendStatus(200);
+    }
+  );
 });
 
-app.post('/api/device/:deviceId/method/:deviceMethod', function (req, res) {
-  debug('device method requested');
+app.post("/api/device/:deviceId/method/:deviceMethod", function(req, res) {
+  debug("device method requested");
   var methodParams = {
     methodName: req.params.deviceMethod,
-    payload: 'hello world',
+    payload: "hello world",
     timeoutInSeconds: 30
   };
 
-  client.invokeDeviceMethod(req.params.deviceId, methodParams, function (err, result) {
+  client.invokeDeviceMethod(req.params.deviceId, methodParams, function(
+    err,
+    result
+  ) {
     if (err) {
-        console.error('Failed to invoke method \'' + methodParams.methodName + '\': ' + err.message);
-        res.sendStatus(500);
+      console.error(
+        "Failed to invoke method '" +
+          methodParams.methodName +
+          "': " +
+          err.message
+      );
+      res.sendStatus(500);
     } else {
-        debug(methodParams.methodName + ' on ' + req.params.deviceId + ':');
-        debug(JSON.stringify(result, null, 2));
-        res.sendStatus(200);
+      debug(methodParams.methodName + " on " + req.params.deviceId + ":");
+      debug(JSON.stringify(result, null, 2));
+      res.sendStatus(200);
     }
   });
 });
 
-io.on('connection', function(socket){
-  debug('a user connected');
-  socket.emit('hello');
+io.on("connection", function(socket) {
+  debug("a user connected");
+  socket.emit("hello");
 });
 
-receiver.on('data', (message) => {
-  debug('firehose:', message.body); 
-  io.sockets.emit('message', message);
+receiver.on("data", message => {
+  debug("firehose:", message.body);
+  io.sockets.emit("message", message);
 });
 
-receiver.on('error', (error) => {
+receiver.on("error", error => {
   console.error(error);
 });
-
