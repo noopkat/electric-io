e74caf7c6c1bc2ae742a529bfe9697eef2dc3b7e
Jeff Horner <jhorner3208@gmail.com>
2019-05-19 11:38:00 -0500

    Handle Errors in fetch calls to the api in configuration.js for getDashboard() and getDeviceList() (#115)
    * Updating the fetch calls to the api for getDashboard() and getDeviceList() in configuration.js to handle network errors if response !== 200

--- public/js/lib/configuration.js
+++ public/js/lib/configuration.js
@@ -1,9 +1,33 @@
-export function getDashboard(vm) {
-  return fetch("/api/dashboard").then(r => r.json());
+export function getDashboard(vm, url) {
+  return new Promise((resolve, reject) =>
+    fetch((url = "/api/dashboard")).then(response => {
+      // Promise is resolved if we get a 200 response status code
+      response.ok
+        ? resolve(response.json())
+        : // If the response status ist't a 200, check if it's a 400
+        response.status === 404
+        ? // error response if the response status code is a 404
+          reject(`Request to ${url} failed with 404 Page Not Found`)
+        : // Catch all error response for any other response status code
+          reject(`HTTP error at ${url}: ${response.status}`);
+    })
+  );
 }
 
-export function getDeviceList() {
-  return fetch("/api/devices/list").then(r => r.json());
+export function getDeviceList(url, devices) {
+  return new Promise((resolve, reject) =>
+    fetch((url = "/api/devices/list")).then(response => {
+      // Promise is resolved if we get a 200 response status code
+      response.ok
+        ? resolve(response.json())
+        : // If the response status ist't a 200, check if it's a 400
+        response.status === 404
+        ? // Error response if the response status code is a 404
+          reject(`Request to ${url} failed with 404 Page Not Found`)
+        : // Catch all error response for any other response status code
+          reject(`HTTP error at ${url}: ${response.status}`);
+    })
+  );
 }
 
 export function saveDashboard(dashboard) {
