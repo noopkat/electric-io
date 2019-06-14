9c4dbb27ab3c794e714d53902ada2f7e9a2e04ab
noopkat <suz.hinton@gmail.com>
2019-06-14 11:28:30 -0700

    add alt text support to sticker card

--- public/js/components/StickerSettings.vue
+++ public/js/components/StickerSettings.vue
@@ -4,6 +4,16 @@
       Picture URL
       <input type="text" name="url" v-bind:value="tile.url" />
     </label>
+    <label>
+      Picture Alt Text
+      <a
+        href="https://axesslab.com/alt-texts#lets-create-the-perfect-alt-text"
+        target="_blank"
+        >(what's this?)</a
+      >
+      <textarea name="altText" maxlength="150" v-bind:value="tile.altText" />
+      max length: 150 char
+    </label>
   </div>
 </template>
 
