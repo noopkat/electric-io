8567850b4e4b97288f65a0c89f4274c6d63cc052
Thomas Koster <tkoster@users.noreply.github.com>
2019-07-12 23:03:37 +1000

    Add component test for DataPropertyField

---
+++ public/js/components/specs/DataPropertyField.spec.js
@@ -0,0 +1,63 @@
+import { shallowMount } from "@vue/test-utils";
+import axe from "axe-core";
+import DataPropertyField from "../DataPropertyField";
+
+describe("DataPropertyField", () => {
+  test("component can mount", () => {
+    const wrapper = shallowMountDataPropertyField("property", "");
+
+    expect(wrapper.isVueInstance()).toBeTruthy();
+  });
+
+  test("has one input element with the expected name", () => {
+    const wrapper = shallowMountDataPropertyField("unusual-property", "");
+
+    const inputs = wrapper.findAll("input[name=unusual-property]");
+
+    expect(inputs.exists()).toBe(true);
+    expect(inputs.length).toEqual(1);
+  });
+
+  describe("given valid input", () => {
+    const validValue = "this.is.a.valid.value";
+
+    test("has aria-invalid attribute unset", () => {
+      const wrapper = shallowMountDataPropertyField("property", validValue);
+
+      const input = wrapper.find("input[name=property]");
+
+      expect(input.attributes("aria-invalid")).toBeUndefined();
+    });
+  });
+
+  describe("given invalid input", () => {
+    const invalidValue = "this is not a valid value";
+
+    test("has aria-invalid attribute set to 'true'", () => {
+      const wrapper = shallowMountDataPropertyField("property", invalidValue);
+
+      const input = wrapper.find("input[name=property]");
+
+      expect(input.attributes("aria-invalid")).toEqual("true");
+    });
+  });
+
+  test("verify component is accessible", () => {
+    const wrapper = shallowMountDataPropertyField("property", "value");
+
+    axe.run(wrapper, (err, { violations }) => {
+      expect(err).toBe(null);
+      expect(violations).toHaveLength(0);
+      done();
+    });
+  });
+});
+
+function shallowMountDataPropertyField(name, value) {
+  return shallowMount(DataPropertyField, {
+    propsData: {
+      name: name,
+      value: value
+    }
+  });
+}
