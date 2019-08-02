516351dab3315144282f24e1042178e1e1591283
Jeff Horner <jhorner3208@gmail.com>
2019-08-02 08:00:32 -0500

    added code coverage for BaseCard - work in progress

--- public/js/components/specs/BaseCard.spec.js
+++ public/js/components/specs/BaseCard.spec.js
@@ -5,9 +5,6 @@ import BaseCard from "../BaseCard";
 
 function shallowMountComponent() {
   return shallowMount(BaseCard, {
-    stubs: {
-      "a11y-dialog": true
-    },
     propsData: {
       tile: {
         deviceId: "AZ3166",
@@ -39,16 +36,9 @@ function shallowMountComponent() {
       ],
       editMode: "unlocked"
     },
-    data: () => ({
-      editingCard: false,
-      draggingCard: false,
-      cardHasBeenMoved: false,
-      x: 200,
-      y: 246,
-      offSetX: 0,
-      offSetY: 0,
-      dialog: null
-    })
+    stubs: {
+      "a11y-dialog": true
+    }
   });
 }
 
@@ -67,11 +57,92 @@ describe("BaseCard", () => {
     expect(vm.editingCard).toEqual(false);
     expect(vm.draggingCard).toEqual(false);
     expect(vm.cardHasBeenMoved).toEqual(false);
-    expect(vm.x).toEqual(200);
-    expect(vm.y).toEqual(246);
-    expect(vm.offSetX).toEqual(0);
-    expect(vm.offSetY).toEqual(0);
+    expect(vm.x).toEqual(vm.tile.position[0]);
+    expect(vm.y).toEqual(vm.tile.position[1]);
+    expect(vm.offsetX).toEqual(0);
+    expect(vm.offsetY).toEqual(0);
     expect(vm.dialog).toEqual(null);
+  });
+
+  test("the computed values of the top and left computed method", () => {
+    const { vm } = shallowMountComponent();
+
+    expect(vm.left).toEqual("200px");
+    expect(vm.top).toEqual("246px");
+
+    vm.x = 210;
+    vm.y = 262;
+
+    expect(vm.left).toEqual("210px");
+    expect(vm.top).toEqual("262px");
+  });
+
+  test("the computed value of style", () => {
+    const wrapper = shallowMountComponent();
+    const inlineStyles = {
+      top: wrapper.vm.top,
+      left: wrapper.vm.left,
+      "--card-tile-width": `${wrapper.vm.blockSize[0] *
+        wrapper.vm.tile.size[0]}px`,
+      minHeight: `${wrapper.vm.blockSize[1] * wrapper.vm.tile.size[1]}px`
+    };
+
+    expect(wrapper.vm.style).toEqual(inlineStyles);
+
+    const card = wrapper.find(".card");
+
+    expect(card.attributes().style).toBe(
+      `top: ${inlineStyles.top}; left: ${inlineStyles.left}; min-height: ${inlineStyles.minHeight};`
+    );
+  });
+
+  test("the computed value of childCard", () => {
+    const wrapper = shallowMountComponent();
+
+    expect(wrapper.vm.childCard).toEqual("line-chart-card");
+
+    wrapper.setProps({
+      tile: {
+        deviceId: "AZ3166",
+        deviceMethod: "stop",
+        id: "ac57912f-1a04-4cc2-a587-1bc116e8cc54",
+        lineColor: "#FF6384",
+        position: [200, 246],
+        property: "",
+        size: [2, 1.5],
+        title: "Line Chart",
+        type: "Button",
+        callType: "method"
+      }
+    });
+
+    expect(wrapper.vm.childCard).toEqual("button-card");
+  });
+
+  test("the computed value of showControlers", () => {
+    const wrapper = shallowMountComponent();
+
+    expect(wrapper.vm.showControls).toEqual(true);
+
+    wrapper.setProps({
+      editMode: "locked"
+    });
+
+    expect(wrapper.vm.showControls).toEqual(false);
+  });
+
+  test("the draggingCard watcher", () => {
+    const spy = jest.spyOn(BaseCard.watch, "draggingCard");
+    const { vm } = shallowMountComponent();
+    const event = {
+      target: {
+        tagName: "H2"
+      }
+    };
+
+    vm.startDraggingCard(event);
+
+    expect(spy).toHaveBeenCalled();
   });
 
   test("Axe doesn’t find any violations", async () => {
